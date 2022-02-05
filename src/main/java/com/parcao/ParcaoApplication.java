f2902e090daa2d7874b82bdbc9e58c4eeacd9449
mar0580 <mar0580@gmail.com>
2022-02-05 16:39:46 -0300

    Criando projeto

---
+++ src/main/java/com/parcao/ParcaoApplication.java
@@ -0,0 +1,13 @@
+package com.parcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcaoApplication.class, args);
+	}
+
+}
